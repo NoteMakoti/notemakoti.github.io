aa5f0f1b880c99bdf11c924abb0daa0521a2fc3a
NoteMakoti <stanleymccatty@gmail.com>
2015-05-03 05:54:43 -0400

    Initial Commit

---
+++ js/smash.js
@@ -0,0 +1,18 @@
+$ ('#calc').click(function(){
+   var my_age = $('#my_age').val();
+   var their_age=$('#their_age').val();
+   if (my_age/ 2 + 7 >their_age) {
+      alert("SURVEY SAYS... nah bruh")
+   } else {
+      alert("SURVEY SAYS...yeah bruh")
+   }})
+
+
+//alert("~IS IT WEIRD THAT I'M TRYNA SMASH, OR NAH?~")
+//var my_age = prompt("How old are you?")
+//var their_age = prompt("How old are they?")
+//if (my_age / 2 + 7 > their_age) {
+//   alert("SURVEY SAYS... yeah bruh")
+//} else {
+//   alert("SURVEY SAYS...naw bruh")
+//}
