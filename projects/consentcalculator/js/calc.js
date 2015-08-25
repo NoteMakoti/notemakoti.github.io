1317575bcbb6b47d8b2d3db98fde8ac0cc16a315
NoteMakoti <stanleymccatty@gmail.com>
2015-08-24 20:10:52 -0400

    Jekyll update

--- projects/consentcalculator/js/calc.js
+++
@@ -1,8 +0,0 @@
-$ ('#calc').click(function(){
-   var my_age = $('#my_age').val();
-   var their_age=$('#their_age').val();
-   if (my_age/ 2 + 7 >their_age) {
-      alert("SURVEY SAYS... nah bruh")
-   } else {
-      alert("SURVEY SAYS...yeah bruh")
-   }})
