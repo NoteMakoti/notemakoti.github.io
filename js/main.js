47356ff516d8a0be92e193ab8e104a9198c147a4
NoteMakoti <stanleymccatty@gmail.com>
2016-02-11 20:23:03 -0500

    quick fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-faded = $("ul#faded li").fadeTo(0, 0);
 
-$(window).scroll(function(d,h) {
-    faded.each(function(i) {
-        a = $(this).offset().top + $(this).height();
-        b = $(window).scrollTop() + $(window).height();
-        if (a < b) $(this).fadeTo(500,1);
-    });
-});
